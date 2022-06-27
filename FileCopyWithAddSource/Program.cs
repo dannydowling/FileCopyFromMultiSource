2caf70da88166db8ba50ef4cccab91b1f6854963
danny <danny@HELIBOT>
2022-06-27 13:46:30 -0700

    Add project files.

---
+++ FileCopyWithAddSource/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualBasic.FileIO;
+public class MultiSourceFileCopy
+{
+    public void Main(string[] args, string destination)
+    {
+        GetData(args, destination);
+    }
+
+    public async Task GetData(string[] args, string destination) 
+    { 
+        var sourcePath = new string[args.Length];
+        {
+            CancellationToken ct = new CancellationToken();
+            var options = new ParallelOptions()
+            {
+                MaxDegreeOfParallelism = 20
+            };
+
+           await Parallel.ForEachAsync(new[] { sourcePath }, options, async (sourcePath, ct) => 
+            {
+                Parallel.ForEach(sourcePath, x =>
+                {
+                    FileSystem.CopyDirectory(x, destination,
+                    UIOption.AllDialogs);
+                    
+                });
+                await Task.Yield();
+            });
+        }
+    }
+}
